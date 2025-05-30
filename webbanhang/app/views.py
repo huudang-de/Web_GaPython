a8638bd96ff95b79da4ee12b5d9862495475cb06
huudang-de <dang36390@gmail.com>
2025-05-31 02:42:43 +0700

    Đăng/update-31/5/2025

--- webbanhang/app/views.py
+++ webbanhang/app/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from .models import *
 import json
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+def register(request):
+    form = UserCreationForm
+    if request.method== "POST":
+        form= UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context= {'form':form}
+    return render(request, 'app/register.html', context)
+def login(request):
+    context= {}
+    return render(request, 'app/login.html', context)
 def home(request):
     products = Product.objects.all()
     context = {'products': products}
-    return render(request, 'app/home.html',context)
+    return render(request, 'app/home.html', context)
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
