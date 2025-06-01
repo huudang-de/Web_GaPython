86aa1f96e89b1cb8a9f1bca2cbeaccc42e8a431d
huudang-de <dang36390@gmail.com>
2025-06-02 02:04:05 +0700

    Đăng_update6/2/2025

--- webbanhang/app/views.py
+++ webbanhang/app/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,JsonResponse
 from .models import *
 import json
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 def register(request):
     form = CreateUserForm()
@@ -12,32 +15,58 @@ def register(request):
             form.save()
     context= {'form':form}
     return render(request, 'app/register.html', context)
-def login(request):
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method=='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request,username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else: messages.info(request, 'user or password not correct!')
     context= {}
     return render(request, 'app/login.html', context)
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
 def home(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_items':0,'get_cart_total':0}
+        cartItems = order['get_cart_items']
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products,'cartItems':cartItems}
     return render(request, 'app/home.html', context)
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_items':0,'get_cart_total':0}
-    context = {'items':items, 'order':order}
+        cartItems = order['get_cart_items']
+    context = {'items':items, 'order':order,'cartItems':cartItems}
     return render(request, 'app/cart.html',context)
 def checkout(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_items':0,'get_cart_total':0}
-    context = {'items':items, 'order':order}
+        cartItems = order['get_cart_items']
+    context = {'items':items, 'order':order,'cartItems':cartItems}
     return render(request, 'app/checkout.html',context)
 def updateItem(request):
     data = json.loads(request.body)
